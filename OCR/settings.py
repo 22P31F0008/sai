fc87a21fa1d3b897eafd46900634f1973a195759
sai <saidaliparthi1@gmail.com>
2024-02-24 12:10:29 +0530

    280CT23

--- OCR/settings.py
+++ OCR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o&e6)lgyzwxln8f=$r%pp3(!a5dwbcsql96h(@y=zb0=$dgsu+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
